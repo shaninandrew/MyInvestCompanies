4c2ebb46497f8d13e25a89fc1e09c34a7585c152
shaninandrew <hedgestandart@gmail.com>
2023-06-30 15:50:44 +0300

    Add files via upload
    Update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
@@ -16,7 +17,22 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+SessionOptions ss = new SessionOptions();
+ss.Cookie = new CookieBuilder();
+ss.Cookie.Name = "Session-id";
+ss.Cookie.Expiration = TimeSpan.FromDays(1);
+
+//app.UseSession( ss);
+//app.UseRequestDecompression();
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+string url = "https://localhost:7779";
+
+ProcessStartInfo ps = new ProcessStartInfo();
+ps.UseShellExecute = true;
+ps.FileName = url;
+System.Diagnostics.Process.Start(ps);
 
 app.Run();

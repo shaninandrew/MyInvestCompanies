5aeb530e2d9fa3ecf0f69da47053986ae8a983ea
shaninandrew <hedgestandart@gmail.com>
2023-07-11 20:50:31 +0300

    Редактор сделок

--- модель данных/Functions.cs
+++ модель данных/Functions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MyInvestCompanies.модель_данных
 {
@@ -611,6 +613,11 @@ namespace MyInvestCompanies.модель_данных
                 Console.WriteLine( ex.Message);
             }
         }
+
+
+
+
+       
 
 
         /// <summary>
